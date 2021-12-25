c139fd4bddd42ac6404f143e12579d45031f049d
dexffe <dexffe@gmail.com>
2021-12-25 13:50:18 +0600

    Отработанно фон уровней

--- Pygame.py
+++ Pygame.py
@@ -54,9 +54,37 @@ if __name__ == '__main__':
                     run = False
                 if pygame.Rect.collidepoint(Settings.rect, pygame.mouse.get_pos()):
                     Fon_settings = Image(400, 0, 'Фон настроек.jpg', all_sprites)
-                    Aim_tip = Image(300, -90, 'Тип прицела.png', all_sprites)
+                    Aim_tip = Image(300, -60, 'Тип прицела.png', all_sprites)
                 if pygame.Rect.collidepoint(Levels.rect, pygame.mouse.get_pos()):
-                    print('Levels')
+                    Level_fon = Image(400, 0, 'Фон уровней.jpg', all_sprites)
+
+                    Level_one = Image(450, 50, '1 уровень.png', all_sprites)
+                    Coin_1_lvl1 = Image(650, 50, 'Монетка.png', all_sprites)
+                    Coin_2_lvl1 = Image(750, 50, 'Монетка.png', all_sprites)
+                    Coin_3_lvl1 = Image(850, 50, 'Монетка.png', all_sprites)
+
+                    Level_two = Image(450, 200, '2 уровень.png', all_sprites)
+                    Coin_1_lvl2 = Image(650, 200, 'Монетка.png', all_sprites)
+                    Coin_2_lvl2 = Image(750, 200, 'Монетка.png', all_sprites)
+                    Coin_3_lvl2 = Image(850, 200, 'Монетка.png', all_sprites)
+
+                    Level_three = Image(450, 350, '3 уровень.png', all_sprites)
+                    Coin_1_lvl3 = Image(650, 350, 'Монетка.png', all_sprites)
+                    Coin_2_lvl3 = Image(750, 350, 'Монетка.png', all_sprites)
+                    Coin_3_lvl3 = Image(850, 350, 'Монетка.png', all_sprites)
+
+                    Level_four = Image(450, 500, '4 уровень.png', all_sprites)
+                    Coin_1_lvl4 = Image(650, 500, 'Монетка.png', all_sprites)
+                    Coin_2_lvl4 = Image(750, 500, 'Монетка.png', all_sprites)
+                    Coin_3_lvl4 = Image(850, 500, 'Неактивная монетка.png', all_sprites)
+                    if pygame.Rect.collidepoint(Level_one.rect, pygame.mouse.get_pos()):
+                        print('Level_one')
+                    if pygame.Rect.collidepoint(Level_two.rect, pygame.mouse.get_pos()):
+                        print('Level_two')
+                    if pygame.Rect.collidepoint(Level_three.rect, pygame.mouse.get_pos()):
+                        print('Level_three')
+                    if pygame.Rect.collidepoint(Level_four.rect, pygame.mouse.get_pos()):
+                        print('Level_four')
                 if pygame.Rect.collidepoint(New_game.rect, pygame.mouse.get_pos()):
                     print('New_game')
 
